b37112995de0bfb1d2b9a72f1091224d730c37d7
Mingshen Sun <sunmingshen@baidu.com>
2017-09-08 00:27:27 -0700

    Reorganize the build script, split into packages
    Change-Id: Ie625a57fcecd46aa0e7f93ae15b4f4e63d835d59

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all:
-	./run.sh
+	./x.sh
 build_docker:
-	sudo docker build --rm -t mssun/mesalock-linux .
+	sudo docker build --rm -t mssun/mesalock-linux docker
 run_docker:
 	sudo docker run --rm -it mssun/mesalock-linux
 clean:
 	rm -rf rootfs rootfs.tar.xz
-	rm -rf src
+	rm -rf build
---
+++ x.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -xe
+
+export MAKEFLAGS=-j$(nproc)
+BUILD_DIR=$(pwd)/build
+PACKAGES_DIR=$(pwd)/packages
+DOCKER_DIR=$(pwd)/docker
+mkdir -p $BUILD_DIR
+
+ROOTFS=$(pwd)/rootfs
+
+# create directory
+mkdir -p $ROOTFS
+mkdir -p $ROOTFS/{bin,dev/{null,pts,shm},lib,lib64,mnt,proc,root,sys,tmp}
+
+# build and install pacakges
+
+PACKAGES=(glibc ion coreutils)
+
+for p in $PACKAGES; do
+  source $PACKAGES_DIR/$p/build.sh
+  WORK_DIR=$BUILD_DIR/$p
+  mkdir -p $WORK_DIR
+  cd $WORK_DIR
+  prepare
+  cd $WORK_DIR
+  configure
+  cd $WORK_DIR
+  compile
+  cd $WORK_DIR
+  install
+done
+
+# libgcc is not compiled from source right now
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 $ROOTFS/lib/
+
+tar cvfJ $DOCKER_DIR/rootfs.tar.xz -C $ROOTFS .
