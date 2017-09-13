2fa4d955f1829c1a61f60d4b7282f2b004deee3d
Mingshen Sun <sunmingshen@baidu.com>
2017-09-13 14:43:11 -0700

    Reorganize the build script, use prepare, build, package
    Change-Id: Ieb2422c300a2b87bfc781ca96835dd6094bba96a

--- x.sh
+++ x.sh
@@ -3,37 +3,22 @@
 set -xe
 
 export MAKEFLAGS=-j$(nproc)
-BUILD_DIR=$(pwd)/build
-PACKAGES_DIR=$(pwd)/packages
-DOCKER_DIR=$(pwd)/docker
-mkdir -p $BUILD_DIR
 
-ROOTFS=$(pwd)/rootfs
+rootfs=$(pwd)/rootfs
+dockerdir=$(pwd)/docker
+rm -rf rootfs
 
-# create directory
-mkdir -p $ROOTFS
-mkdir -p $ROOTFS/{bin,dev/{null,pts,shm},lib,lib64,mnt,proc,root,sys,tmp}
+packages=(filesystem glibc ion coreutils ripgrep fd-find)
 
-# build and install pacakges
-
-# PACKAGES=(glibc ion coreutils ripgrep fd-find)
-PACKAGES=(fd-find)
-
-for p in ${PACKAGES[@]}; do
-  source $PACKAGES_DIR/$p/build.sh
-  WORK_DIR=$BUILD_DIR/$p
-  mkdir -p $WORK_DIR
-  cd $WORK_DIR
-  prepare
-  cd $WORK_DIR
-  configure
-  cd $WORK_DIR
-  compile
-  cd $WORK_DIR
-  install
+for p in ${packages[@]}; do
+  ./mkpkg packages/$p/BUILD
 done
 
-# libgcc is not compiled from source right now
-cp /lib/x86_64-linux-gnu/libgcc_s.so.1 $ROOTFS/lib/
+mkdir -p $rootfs
+for p in ${packages[@]}; do
+  tar xvfJ build/out/$p.tar.xz -C rootfs
+done
 
-tar cvfJ $DOCKER_DIR/rootfs.tar.xz -C $ROOTFS .
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 $rootfs/usr/lib/
+
+tar cvfJ $dockerdir/rootfs.tar.xz -C $rootfs .
