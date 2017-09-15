c85b5ac030558c8d76f2a09ef7cc871ba492f219
Mingshen Sun <sunmingshen@baidu.com>
2017-09-15 16:31:13 -0700

    New packages: tokei
    Change-Id: Ibd85cb96e92ebb2a318db350a7b68c607b12a985

--- x.sh
+++ x.sh
@@ -7,7 +7,17 @@ export MAKEFLAGS=-j$(nproc)
 rootfs=$(pwd)/rootfs
 dockerdir=$(pwd)/docker
 
-packages=(filesystem glibc ion uutils ripgrep fd-find tzdata xi-core xi-tui)
+packages=(filesystem \
+          glibc \
+          ion \
+          uutils \
+          ripgrep \
+          fd-find \
+          tzdata \
+          xi-core \
+          xi-tui \
+          findutils \
+          tokei)
 
 for p in ${packages[@]}; do
   ./mkpkg $p
@@ -16,8 +26,9 @@ done
 rm -rf $rootfs
 mkdir -p $rootfs
 for p in ${packages[@]}; do
-  tar xvfJ build/out/$p.tar.xz -C rootfs
+  tar xvfJ build/out/$p.tar.xz -C rootfs &
 done
+wait
 
 install -D -m755 /lib/x86_64-linux-gnu/libgcc_s.so.1 -t $rootfs/usr/lib/
 
