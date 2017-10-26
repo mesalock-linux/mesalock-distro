a79d28dbbc19ca7232296442e8bdf64f79e7b8bd
Mingshen Sun <sunmingshen@baidu.com>
2017-10-26 15:21:20 -0700

    Cleanup x.sh
    Change-Id: I9e64c5bd399711fd97d05c9b2ba9f4b3e075076f

--- x.sh
+++ x.sh
@@ -2,36 +2,9 @@
 
 set -xe
 
-export MAKEFLAGS=-j$(nproc)
+pkgrepo=$(pwd)/packages
 
-rootfs=$(pwd)/rootfs
-dockerdir=$(pwd)/docker
-
-packages=(filesystem \
-          glibc \
-          ion-shell \
-          uutils \
-          ripgrep \
-          fd-find \
-          tzdata \
-          xi-core \
-          xi-tui \
-          findutils \
-          tokei \
-          brotli \
-          micro)
-
-for p in ${packages[@]}; do
+for p in `ls $pkgrepo`; do
+  echo "[+] Building $p"
   ./mkpkg $p
 done
-
-rm -rf $rootfs
-mkdir -p $rootfs
-for p in ${packages[@]}; do
-  tar xvfJ build/out/$p.tar.xz -C rootfs &
-done
-wait
-
-install -D -m755 /lib/x86_64-linux-gnu/libgcc_s.so.1 -t $rootfs/usr/lib/
-
-tar cvfJ $dockerdir/rootfs.tar.xz -C $rootfs .
